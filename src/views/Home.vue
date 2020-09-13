78934a8cc0db38420f6bf1bc481c230f9aeb2345
19971202 <t.takahashi19971202@gmail.com>
2020-09-13 20:54:45 +0900

    web応用

---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex">
+    <div class="left">
+      <SideNavi />
+    </div>
+    <div class="right">
+      <div class="title">
+        <p>ホーム</p>
+      </div>
+      <Message />
+    </div>
+  </div>
+</template>
+
+<script>
+import SideNavi from "../components/SideNavi";
+import Message from "../components/Message";
+export default {
+  components: {
+    SideNavi,
+    Message
+  }
+};
+</script>
+
+<style scoped>
+.left {
+  width: 22%;
+  height: 100vh;
+}
+
+.flex {
+  display: flex;
+}
+
+.right {
+  width: 78%;
+  height: 100vh;
+}
+
+.title p {
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.title {
+  border-bottom: 1px solid white;
+  border-left: 1px solid white;
+  padding: 15px;
+}
+</style>
